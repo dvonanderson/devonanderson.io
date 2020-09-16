7e88bc95bb2953f5c5f4f23441fe56a530777658
Devon Anderson <dvonanderson@gmail.com>
2020-09-16 10:46:03 -0400

    configured deploy variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://devonanderson-io-api.herokuapp.com"
+          : "http://localhost:1337" ,
         queryLimit: 1000, // Default to 100
         contentTypes: [`jobs`, `projects`, `blogs`],
         singleTypes: [`about`],

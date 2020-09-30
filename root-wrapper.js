18914374f2e0c0b843ea0fa7f309a45a6aafa15e
Devon Anderson <dvonanderson@gmail.com>
2020-09-30 12:48:24 -0400

    created root-wrapper js for ssr

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,31 +1,25 @@
 import React from 'react'
 import Layout from './src/components/Layout'
 import { GlobalStyles } from './src/themes/globalStyles'
-import * as theme from "themes/theme"
-import  CookieConsent,{ Cookies } from "react-cookie-consent"
-import "./root-wrapper.css"
+import * as theme from 'themes/theme'
 import { ThemeProvider } from 'styled-components'
+import Cookie from './src/components/Cookies'
+import { MDXProvider } from '@mdx-js/react'
+import { Headings, Code, MdxBlockQuote, Prism } from './src/components/Mdx'
 
-export const wrapRootElement = ({element}) => (
+const components = {
+  h2: Headings.myH2,
+  h4: Headings.myH4,
+  inlineCode: Code,
+  blockquote: MdxBlockQuote,
+  pre: Prism,
+}
+export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <CookieConsent
-      disableStyles={true}
-      containerClasses="containerClass"
-      contentClasses="contentClass"
-      buttonWrapperClasses="buttonWrapperClass"
-      buttonClasses="buttonClass" 
-      onAccept={() => {
-        Cookies.set("gatsby-gdpr-google-analytics", "gatsby-gdpr-facebook-pixel")
-      }}
-      debug={true}
-      location="bottom"
-      buttonText="Got it"
-    >
-      <h4>Cookie Consent</h4>
-      This website uses cookies that help the website function and to help us understand how to interact with it.  We use These cookies to provide you with improved and customized user-experience.  To find out more about the cookies we use, see our Privacy Policy.
-    </CookieConsent>
-    <Layout>{element}</Layout>
+    <Cookie />
+    <MDXProvider components={components}>
+      <Layout>{element}</Layout>
+    </MDXProvider>
   </ThemeProvider>
 )
-

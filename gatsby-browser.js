d9a341278957cf8b87e364366509d0b79619ffe0
Devon Anderson <dvonanderson@gmail.com>
2020-10-01 14:33:23 -0400

    testing low speed browser spinner

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
 import { wrapRootElement as wrap } from './root-wrapper'
 
 export const wrapRootElement = wrap
+
+export const onInitialClientRender = () => {
+  setTimeout(function () {
+    document.getElementById('___loader').style.display = 'none'
+  }, 3000)
+}
---
+++ src/html.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LoaderSVG from './assets/dsa_logo_animated.svg'
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`loader`}
+          id="___loader"
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            background: '#f2f2f2',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 100,
+          }}
+        >
+          <img src={LoaderSVG} alt="loading icon" height="1080" width="1920" />
+        </div>
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
